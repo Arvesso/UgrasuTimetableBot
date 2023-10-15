f86847b7048a00b1b994c4024ab0d8a82b13e6d4
Arvesso <artemcss02@gmail.com>
2023-10-16 00:57:25 +0300

    build-structure-stage2

---
+++ UgrasuTimetableBot/IOControl/InMemoryStorage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UgrasuTimetableBot.Schedule;
+
+namespace UgrasuTimetableBot.IOControl
+{
+    public class InMemoryStorage
+    {
+        public InMemoryStorage()
+        {
+            
+        }
+
+        private List<Group> Groups { get; init; } = new();
+        private List<Tutor> Tutors { get; init; } = new();
+        private List<string> Faculties { get; init; } = new();
+
+        public void AddGroup(Group group) => Groups.Add(group);
+        public void AddTutor(Tutor tutor) => Tutors.Add(tutor);
+        public void AddFaculty(string faculty) => Faculties.Add(faculty);
+    }
+}
